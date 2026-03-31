214f34cfdf0cfa2e5f089d5e55818dcc08e2ac25
Elias13840 <asuntagallegos15@gmail.com>
2026-03-30 23:21:39 -0500

    Create AirBlock.java

---
+++ Blocks/AirBlock.java
@@ -0,0 +1,23 @@
+public class AirBlock extends Block {
+     public AirBlock() {
+         id = "air-block";
+         name = "Aire";
+         width = 32;
+         height = 32;
+         textureResId = 0;
+         backgroundColor = 0x00000000;
+         borderColor = 0x00000000;
+         weight = 0.0f;
+         isSolid = false;
+         isTransparent = true;
+         hasGravity = false;
+         hardness = 0;
+         harvestLevel = 0;
+         optimalToolType = "";
+         dropItem = null;
+         maxStack = 0;
+         isOre = false;
+         isCraftable = false;
+         buildTime = 0;
+     }
+}
