a9fd282ff1d032cad5813827bf2a412289719525
Elias13840 <asuntagallegos15@gmail.com>
2026-03-30 23:17:11 -0500

    Add StoneBlock class with properties and methods

---
+++ Blocks/Stone.java
@@ -0,0 +1,23 @@
+public class StoneBlock extends Block {
+     public StoneBlock() {
+         id = "stone-block";
+         name = "Piedra";
+         width = 32;
+         height = 32;
+         textureId = 1;
+         backgroundColor = 0xFFA9A9A9;
+         borderColor = 0xFF000000;
+         weight = 1.5f;
+         isSolid = true;
+         isTransparent = false;
+         hasGravity = false;
+         hardness = 10;
+         harvestLevel = 1;
+         optimalToolType = "pickaxe"; // Herramienta ideal: pico
+         dropItem = new StoneItem();
+         maxStack = 64;
+         isOre = false;
+         isCraftable = false;
+         buildTime = 1000;
+     }
+}
